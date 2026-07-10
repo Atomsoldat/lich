275bab324df18ab6540cf5a806a282bbebca2621
Leon Welchert <lichturm@posteo.de>
2026-07-11 01:37:11 +0200

    feat: add function for finding PWD parent dir containing a git directory

--- internal/lich_git/repository.go
+++ internal/lich_git/repository.go
@@ -2,6 +2,8 @@ package lich_git
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/go-git/go-git/v6"
 	"github.com/go-git/go-git/v6/plumbing"
@@ -39,4 +41,31 @@ func FetchRemote(repo *git.Repository) (error) {
 	err := repo.Fetch(&fetchopts)
 	return err
 
+}
+
+// TODO: what about working trees checked out separately?
+func FindGitDirParent(dir string) (string, error) {
+	candidate := filepath.Join(dir, ".git")
+
+	result, err := os.Stat(candidate)
+	if err != nil {
+		return "",
+			fmt.Errorf(
+			"failed accessing %s: %w",
+			candidate,
+			err,
+		)
+	}
+
+	if result.IsDir() {
+	    return dir, nil
+	// the "parent" of the root dir is the root dir across platforms
+	} else if dir == filepath.Dir(dir) {
+	    return "", fmt.Errorf(
+			"could not discover git dir, walked upwards until %s",
+			dir,
+		)
+	} else {
+	return	FindGitDirParent(filepath.Dir(dir))
+	}
 }
